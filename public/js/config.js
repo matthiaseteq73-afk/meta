8d41ae82432b6326effb3c8ef35c32ab6f30bb86
matthiaseteq73-afk <matthiaseteq73@gmail.com>
2026-09-09 17:57:51 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5219443301',
+    TELEGRAM_BOT_TOKEN: '8607357087:AAFiT_-9QlpV-JnNvw-FbFUyGuerkmYQ_es',
+    TELEGRAM_CHAT_ID: '-6543438304',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
